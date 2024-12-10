acc24f14e67d6b921ad66acee4862afc300472bd
Christian Oliveira <christianoliveira1908@hotmail.com>
2024-12-09 21:58:50 -0300

    Configurado context.

---
+++ RazorLibrary.Infra/Database/BookContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RazorLibrary.Infra.Database
+{
+    public class BookContext : DbContext
+    {
+        public BookContext(DbContextOptions<BookContext> options) 
+        {
+            
+        }
+    }
+}
---
+++ RazorLibrary.Infra/InfraExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RazorLibrary.Infra.Database;
+
+namespace RazorLibrary.Infra
+{
+    public static class InfraExtensions
+    {
+
+        public static void AddInfra(this IServiceCollection services, IConfiguration configuration)
+        {
+            AddSqlContext(services, configuration);
+        }
+
+        private static void AddSqlContext(IServiceCollection service, IConfiguration configuration)
+        {
+            service.AddDbContext<BookContext>(opt =>
+            {
+                var connectionString = configuration.GetConnectionString("SqlServer");
+
+                opt.UseSqlServer(connectionString);
+            });
+        }
+    }
+}
