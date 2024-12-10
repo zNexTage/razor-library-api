f89c1f1cbc1293c5da90fc777a111b1624f7231a
Christian Oliveira <christianoliveira1908@hotmail.com>
2024-12-09 21:27:54 -0300

    Criado API (porta primária)
    Ponto de entrada da aplicação (driver).

---
+++ RazorLibrary.API/Controllers/Book/BookController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RazorLibrary.API.Controllers.Book
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {        
+
+    }
+}
