69ef7251510696df51989edea7bd4c3b17033445
Christian Oliveira <christianoliveira1908@hotmail.com>
2024-12-11 21:59:31 -0300

    Configurado migrations.

--- RazorLibrary.Infra/InfraExtensions.cs
+++ RazorLibrary.Infra/InfraExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using FluentMigrator.Runner;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using RazorLibrary.Infra.Database;
@@ -11,6 +12,10 @@ namespace RazorLibrary.Infra
         public static void AddInfra(this IServiceCollection services, IConfiguration configuration)
         {
             AddSqlContext(services, configuration);
+
+            AddFluentMigration(services, configuration);
+
+            MigrateDatabase(services);
         }
 
         private static void AddSqlContext(IServiceCollection service, IConfiguration configuration)
@@ -21,6 +26,31 @@ namespace RazorLibrary.Infra
 
                 opt.UseSqlServer(connectionString);
             });
+        }
+
+        private static void AddFluentMigration(IServiceCollection service, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("SqlServer");
+
+            service.AddFluentMigratorCore()
+                .ConfigureRunner(rb => rb
+                    .AddSqlServer()
+                    .WithGlobalConnectionString(connectionString)
+                    .ScanIn(typeof(InfraExtensions).Assembly).For.Migrations())
+                .AddLogging(lb => lb.AddFluentMigratorConsole())
+                .BuildServiceProvider(false);                
+        }
+
+        private static void MigrateDatabase(IServiceCollection service)
+        {
+            var provider = service.BuildServiceProvider();
+
+            using(var scope = provider.CreateScope())
+            {
+                var runner = scope.ServiceProvider.GetRequiredService<IMigrationRunner>();
+
+                runner.MigrateUp();
+            }
         }
     }
 }
