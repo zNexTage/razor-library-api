fc8e41bb16494e36ba0512886c07445ac7ca8a4b
Christian Oliveira <christianoliveira1908@hotmail.com>
2024-12-11 22:00:44 -0300

    Criado migration para Book
    Criando tabela Book.

---
+++ RazorLibrary.Infra/Migrations/1_AddBookTable.cs
@@ -0,0 +1,29 @@
+﻿using FluentMigrator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RazorLibrary.Infra.Migrations
+{
+    [Migration(1)]
+    public class AddBookTable : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("Books")
+                .WithColumn("Id").AsGuid().PrimaryKey().Unique()
+                .WithColumn("Title").AsString().NotNullable()
+                .WithColumn("Publisher").AsString(70).NotNullable()
+                .WithColumn("Photo").AsString(1000).NotNullable()
+                .WithColumn("Authors").AsString(500).NotNullable();
+
+        }
+
+        public override void Down()
+        {
+            Delete.Table("Books");
+        }
+    }
+}
