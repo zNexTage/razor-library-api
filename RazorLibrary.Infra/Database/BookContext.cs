e4fef8ca0269e997973f53cd3ea89cd8a991c8a9
Christian Oliveira <christianoliveira1908@hotmail.com>
2024-12-23 18:59:20 -0300

    Adicionado Book em context.

--- RazorLibrary.Infra/Database/BookContext.cs
+++ RazorLibrary.Infra/Database/BookContext.cs
@@ -1,12 +1,15 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using RazorLibrary.Domain.Entities;
 
 namespace RazorLibrary.Infra.Database
 {
     public class BookContext : DbContext
     {
-        public BookContext(DbContextOptions<BookContext> options) 
+        public BookContext(DbContextOptions<BookContext> options)  : base(options)
         {
             
         }
+
+        public DbSet<Book> Books { get; set; }
     }
 }
