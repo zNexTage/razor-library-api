8a1568458d976d47c5620252eb2aaf9f86bb9ff1
Christian Oliveira <christianoliveira1908@hotmail.com>
2024-12-23 19:00:40 -0300

    Adicionado injeção da camada Application

--- RazorLibrary.API/Program.cs
+++ RazorLibrary.API/Program.cs
@@ -1,4 +1,5 @@
 using RazorLibrary.Infra;
+using RazorLibrary.Application;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +14,9 @@ var configuration = builder.Configuration;
 
 // Adiciona infra
 builder.Services.AddInfra(configuration);
+
+// Adiciona application
+builder.Services.AddApplication();
 
 var app = builder.Build();
 
