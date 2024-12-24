46e04223c0091fbe9e653c17276103e70c5ff13a
Christian Oliveira <christianoliveira1908@hotmail.com>
2024-12-24 18:01:08 -0300

    Criado extension para camada Application

---
+++ RazorLibrary.Application/ApplicationExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using RazorLibrary.Application.Services.Book;
+using RazorLibrary.Domain.Adapters.Services.Book;
+
+namespace RazorLibrary.Application
+{
+    public static class ApplicationExtensions
+    {
+        public static void AddApplication(this IServiceCollection service)
+        {
+            AddServices(service);
+        }
+
+        private static void AddServices(IServiceCollection service)
+        {
+            service.AddScoped<IWriteBookService, WriteBookService>();
+        }
+    }
+}
