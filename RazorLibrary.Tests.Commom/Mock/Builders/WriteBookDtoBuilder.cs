51615e484d43744e18824d6420ca91e3230f13fa
Christian Oliveira <christianoliveira1908@hotmail.com>
2024-12-24 17:58:35 -0300

    Criado Builder para WriteBookDto

---
+++ RazorLibrary.Tests.Commom/Mock/Builders/WriteBookDtoBuilder.cs
@@ -0,0 +1,28 @@
+﻿using Bogus;
+using RazorLibrary.Domain.DataTransferObject.Book;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RazorLibrary.Tests.Commom.Mock.Builders
+{
+    public static class WriteBookDtoBuilder
+    {
+        public static WriteBookDto Build()
+        {
+            var faker = new Faker();
+
+            return new WriteBookDto()
+            {
+                Authors = {
+                    faker.Person.FullName
+                },
+                Photo = faker.Image.PicsumUrl(),
+                Publisher = faker.Company.CompanyName(),
+                Title = faker.Commerce.ProductName()
+            };
+        }
+    }
+}
