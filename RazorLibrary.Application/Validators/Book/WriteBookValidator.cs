396844146a210d76ec25e0175c156653c97ef93f
Christian Oliveira <christianoliveira1908@hotmail.com>
2024-12-24 17:59:09 -0300

    Criado validator para WriteBookDto

---
+++ RazorLibrary.Application/Validators/Book/WriteBookValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using RazorLibrary.Domain.DataTransferObject.Book;
+
+namespace RazorLibrary.Application.Validators.Book
+{
+    public class WriteBookValidator : AbstractValidator<WriteBookDto>
+    {
+        public WriteBookValidator()
+        {
+            RuleFor(b => b.Title).NotEmpty().WithMessage("Por favor, informe o título do livro");
+
+            RuleFor(b => b.Publisher).NotEmpty().WithMessage("Por favor, informe a editora");
+
+            RuleFor(b => b.Photo).NotEmpty().WithMessage("Por favor, informe a foto do livro");
+
+            RuleFor(b => b.Authors)
+                .NotEmpty().WithMessage("Por favor, informe pelo menos 1 autor.");
+
+            RuleForEach(b => b.Authors)
+                .NotEmpty().WithMessage("Por favor, informe o nome do autor");
+        }
+    }
+}
