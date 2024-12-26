245b7e20be4f4a2a0ade5171085b2c28252ec34e
Christian Oliveira <christianoliveira1908@hotmail.com>
2024-12-26 13:34:04 -0300

    Alterado tamanho da propriedade Title de Book.

---
+++ RazorLibrary.Infra/Migrations/2_AlterSizeTitle.cs
@@ -0,0 +1,18 @@
+﻿using FluentMigrator;
+
+namespace RazorLibrary.Infra.Migrations
+{
+    [Migration(2)]
+    public class AlterSizeTitle : Migration
+    {
+        public override void Down()
+        {
+            Alter.Table("Books").AlterColumn("Title").AsString(0);
+        }
+
+        public override void Up()
+        {
+            Alter.Table("Books").AlterColumn("Title").AsString(70);
+        }
+    }
+}
