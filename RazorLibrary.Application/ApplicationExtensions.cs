3892da6ea7fe6c5e6c5f47e7a32e587615b576c5
Christian Oliveira <christianoliveira1908@hotmail.com>
2024-12-26 16:25:43 -0300

    Criado Mapper para Book.

--- RazorLibrary.Application/ApplicationExtensions.cs
+++ RazorLibrary.Application/ApplicationExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using RazorLibrary.Application.Services.Book;
 using RazorLibrary.Domain.Adapters.Services.Book;
+using System.Reflection;
 
 namespace RazorLibrary.Application
 {
@@ -9,12 +10,20 @@ namespace RazorLibrary.Application
         public static void AddApplication(this IServiceCollection service)
         {
             AddServices(service);
+            AddMapper(service);
         }
 
         private static void AddServices(IServiceCollection service)
         {
             service.AddScoped<IWriteBookService, WriteBookService>();
             service.AddScoped<IReadBookService, ReadBookService>();
+        }
+
+        private static void AddMapper(IServiceCollection service)
+        {
+            var assembly = typeof(ApplicationExtensions).Assembly;
+
+            service.AddAutoMapper(assembly);
         }
     }
 }
---
+++ RazorLibrary.Application/Mapper/Book/BookProfile.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using RazorLibrary.Domain.DataTransferObject.Book;
+using RazorLibrary.Domain.Entities;
+
+namespace RazorLibrary.Application.Mapper.Book
+{
+    using RazorLibrary.Domain.Entities;
+    using System.Linq;
+
+    public class BookProfile : Profile
+    {
+        public BookProfile()
+        {
+            CreateMap<WriteBookDto, Book>()
+                .ForMember(dto => dto.Authors, 
+                opt => opt.MapFrom(dto => String.Join(", ", dto.Authors)));
+
+            CreateMap<Book, ReadBookDto>()
+                .ForMember(dto => dto.Id,
+                opt => opt.MapFrom(book => book.Id.ToString()))
+                .ForMember(dto => dto.Authors,
+                opt => opt.MapFrom(book => SplitAuthors(book.Authors)));
+        }
+
+        public List<string> SplitAuthors(string authors) => authors.Split(", ").ToList();
+    }
+}
