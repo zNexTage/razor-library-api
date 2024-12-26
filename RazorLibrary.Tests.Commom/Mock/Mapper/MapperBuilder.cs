310db532d391f2026617919bd18c2c6b6adda46f
Christian Oliveira <christianoliveira1908@hotmail.com>
2024-12-26 16:29:48 -0300

    Criado builders para Mapper e ReadBookRepository.

---
+++ RazorLibrary.Tests.Commom/Mock/Mapper/MapperBuilder.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using RazorLibrary.Application.Mapper.Book;
+
+namespace RazorLibrary.Tests.Commom.Mock.Mapper
+{
+    public class MapperBuilder
+    {
+        public static IMapper Build()
+        {
+            return new AutoMapper.MapperConfiguration(options =>
+            {
+                options.AddMaps(typeof(BookProfile).Assembly);
+            }).CreateMapper();
+        }
+    }
+}
---
+++ RazorLibrary.Tests.Commom/Mock/Repositories/Book/ReadBookRepositoryBuilder.cs
@@ -0,0 +1,22 @@
+﻿using Moq;
+using RazorLibrary.Domain.Adapters.Repositories.Book;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RazorLibrary.Tests.Commom.Mock.Repositories.Book
+{
+    public class ReadBookRepositoryBuilder
+    {
+        public static IReadBookRepository Build(bool existsReturn = true)
+        {
+            var mock = new Mock<IReadBookRepository>();
+
+            mock.Setup(m => m.Exists(It.IsAny<string>())).ReturnsAsync(existsReturn);
+
+            return mock.Object;
+        }
+    }
+}
