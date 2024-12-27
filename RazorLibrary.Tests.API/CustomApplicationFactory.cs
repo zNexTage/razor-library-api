1703471d529a4b4a88ef8a49687daa2b357cf946
Christian Oliveira <christianoliveira1908@hotmail.com>
2024-12-27 15:18:04 -0300

    Criado startup customizado para testes.

---
+++ RazorLibrary.Tests.API/CustomApplicationFactory.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using RazorLibrary.Domain.Entities;
+using RazorLibrary.Infra.Database;
+using RazorLibrary.Tests.Commom.Seeds;
+
+namespace RazorLibrary.Tests.API
+{
+    
+    public class CustomApplicationFactory : WebApplicationFactory<Program>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder
+                .UseEnvironment("Test")
+                .ConfigureServices(services =>
+            {
+                var  dbContextDescriptor = services.FirstOrDefault(d => d.ServiceType == typeof(DbContextOptions<BookContext>));
+
+                if(dbContextDescriptor is not null)
+                {
+                    services.Remove(dbContextDescriptor);
+                }
+
+                var provider = services.AddEntityFrameworkInMemoryDatabase().BuildServiceProvider();
+
+                services.AddDbContext<BookContext>(opt =>
+                {
+                    opt.UseInMemoryDatabase("InMemoryDbForTesting");
+                    opt.UseInternalServiceProvider(provider);
+                });
+
+                services.AddScoped<SeedBook>();
+
+                var serviceProvider = services.BuildServiceProvider();
+
+                var seedBook = serviceProvider.GetRequiredService<SeedBook>();
+
+                seedBook.SeedData().Wait();
+            });
+        }
+    }
+}
