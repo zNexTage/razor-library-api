2e25283308c8518f851b9d8ce3aa8e73926a1650
Christian Oliveira <christianoliveira1908@hotmail.com>
2024-12-27 11:33:38 -0300

    Removido usings que não estavam sendo usados.

--- RazorLibrary.Tests.Commom/Mock/Builders/WriteBookDtoBuilder.cs
+++ RazorLibrary.Tests.Commom/Mock/Builders/WriteBookDtoBuilder.cs
@@ -1,10 +1,5 @@
 ﻿using Bogus;
 using RazorLibrary.Domain.DataTransferObject.Book;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RazorLibrary.Tests.Commom.Mock.Builders
 {
